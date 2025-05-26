50578072bf568fcd3362320d1f57f7902428b1c6
Nguyen Van Tri <vantri1010@gmail.com>
2025-05-26 23:00:31 +0700

    67. Add Binary

---
+++ 67. Add Binary/AddBinary.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder output = new StringBuilder();
+
+        int carry = 0;
+        int aLen = a.length() - 1;
+        int bLen = b.length() - 1;
+
+        while( aLen >= 0 || bLen >= 0 || carry ==1 ){
+            if (aLen >= 0){
+                carry += a.charAt(aLen) - '0';
+                aLen --;
+            } 
+
+            if (bLen >= 0) {
+                carry += b.charAt(bLen) - '0';
+                bLen --;
+            }
+
+            output.insert(0, carry % 2);
+            carry /= 2;
+        }
+
+        return output.toString();
+    }
+}
