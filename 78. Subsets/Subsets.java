86352128837e853f39905f4d4d487148e755c399
Nguyen Van Tri <vantri1010@gmail.com>
2025-06-01 23:51:08 +0700

    improved java version

--- 78. Subsets/Subsets.java
+++ 78. Subsets/Subsets.java
@@ -1,16 +1,16 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> output = new ArrayList<>();
-        helper(nums, output, 0, new ArrayList<Integer>());
+        backtrack(nums, output, 0, new ArrayList<>());
         return output;
     }
 
-    public void helper(int[] nums, List<List<Integer>> output, int index, List<Integer> cur) {
-        output.add(new ArrayList<>(cur));
-        for (int i = index; i < nums.length; i++) {
-            cur.add(nums[i]);
-            helper(nums, output, i + 1, cur);
-            cur.remove(cur.size() - 1);
+    private void backtrack(int[] nums, List<List<Integer>> output, int start, List<Integer> path) {
+        output.add(new ArrayList<>(path)); // Add a copy of the current subset
+        for (int i = start; i < nums.length; i++) {
+            path.add(nums[i]); // Include nums[i]
+            backtrack(nums, output, i + 1, path); // Recurse
+            path.remove(path.size() - 1); // Backtrack (exclude nums[i])
         }
     }
 }
