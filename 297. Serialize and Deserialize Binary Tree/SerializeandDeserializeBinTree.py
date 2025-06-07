49938d53a2cea35f1d7f75be466a5938603da1a0
Nguyen Van Tri <vantri1010@gmail.com>
2025-06-08 00:01:38 +0700

    update python version

--- 297. Serialize and Deserialize Binary Tree/SerializeandDeserializeBinTree.py
+++ 297. Serialize and Deserialize Binary Tree/SerializeandDeserializeBinTree.py
@@ -5,47 +5,32 @@
 #         self.left = None
 #         self.right = None
 
-public class Codec {
-    private static final String NULL_MARKER = "#";
-    private static final String DELIMITER = ",";
-    
-    // Encodes a tree to a single string.
-    public String serialize(TreeNode root) {
-        StringBuilder sb = new StringBuilder();
-        serializeHelper(root, sb);
-        return sb.toString();
-    }
-    
-    private void serializeHelper(TreeNode node, StringBuilder sb) {
-        if (node == null) {
-            sb.append(NULL_MARKER).append(DELIMITER);
-            return;
-        }
+class Codec:
+    def serialize(self, root):
+        def preorder(node):
+            if not node:
+                vals.append("#")
+            else:
+                vals.append(str(node.val))
+                preorder(node.left)
+                preorder(node.right)
         
-        sb.append(node.val).append(DELIMITER);
-        serializeHelper(node.left, sb);
-        serializeHelper(node.right, sb);
-    }
+        vals = []
+        preorder(root)
+        return ",".join(vals)
     
-    // Decodes your encoded data to tree.
-    public TreeNode deserialize(String data) {
-        LinkedList<String> queue = new LinkedList<>(Arrays.asList(data.split(DELIMITER)));
-        return deserializeHelper(queue);
-    }
-    
-    private TreeNode deserializeHelper(LinkedList<String> queue) {
-        String val = queue.poll();
+    def deserialize(self, data):
+        def build():
+            val = next(vals)
+            if val == "#":
+                return None
+            node = TreeNode(int(val))
+            node.left = build()
+            node.right = build()
+            return node
         
-        if (val.equals(NULL_MARKER)) {
-            return null;
-        }
-        
-        TreeNode node = new TreeNode(Integer.parseInt(val));
-        node.left = deserializeHelper(queue);
-        node.right = deserializeHelper(queue);
-        return node;
-    }
-}
+        vals = iter(data.split(","))
+        return build()
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
